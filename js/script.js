807cd14d5e8ceb5f9e910c850688310af8a4a5c9
HonoreSolomon <HonoreSolomon@hotmail.com>
2022-07-12 16:30:08 -0700

    finished layout

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const contactButton = document.querySelector("#contact-button");
+const contactDialog = document.querySelector("#contact-dialog");
+
+contactButton.addEventListener("click", () => {
+  contactDialog.showModal();
+});
