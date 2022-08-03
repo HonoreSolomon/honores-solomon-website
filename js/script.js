e190cbb86d4c1af74d4497673e8953f8d527054c
HonoreSolomon <HonoreSolomon@hotmail.com>
2022-08-03 10:17:08 -0700

    creates responsive breakpoints and resized fonts and aside link sizing. also moved aside to fit semantics.

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// const contactButton = document.querySelector("#contact-button");
-// const contactDialog = document.querySelector("#contact-dialog");
-
-// contactButton.addEventListener("click", () => {
-//   contactDialog.showModal();
-// });
